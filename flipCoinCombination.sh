68c20e8ac3c9689002fffe1f84bd2e17de052ce0
sdmkhan123 <sdmkhan126410@gmail.com>
2021-08-16 01:33:40 +0530

    Percentage of head and Tail in n number of iteration

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,10 +1,22 @@
 #! /bin/bash -x
-
-flip=$((RANDOM%2))
-if [ $flip -eq 1 ]
-then
-	echo "Head"
-else
-	echo "Tail"
-fi
-
+HeadCount=0
+TailCount=0
+declare -A Dictionary
+read num
+for (( i=0; i<num; i++ ))
+do
+	flip=$((RANDOM%2))
+	if [ $flip -eq 0 ]
+	then
+		HeadCount=$(( HeadCount + 1 ))
+	else
+		TailCount=$(( TailCount + 1 ))
+	fi
+done
+totalFlip=$(( HeadCount + TailCount ))
+Dictionary[0]=$HeadCount
+Dictionary[1]=$TailCount
+HeadPercentage=$( echo $HeadCount $totalFlip | awk '{ print 100 * $1/$2 }' )
+TailPercentage=$( echo $TailCount $totalFlip | awk '{ print 100 * $1/$2 }' )
+echo "Head Percentage is : $HeadPercentage %"
+echo "Tail Percentage is : $TailPercentage %"
